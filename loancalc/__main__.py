7e83e5335418474fc1f4510e06d77120dadc8abe
Corey S. Seliger <corey@seligerfamily.org>
2023-04-04 20:27:05 -0400

    Initial Commit

---
+++ loancalc/__main__.py
@@ -0,0 +1,45 @@
+
+import logging
+import atexit
+import sys
+
+from loancalc.application import LoanCalc
+
+APP_NAME = "Loan Calculator"
+APP_EXE_NAME = "loancalc"
+APP_VERSION = "0.0.1"
+
+# Helper function to ensure we capture stack traces via logging
+# (Note: This method does not work with threads until Python 3.8)
+def log_excepthook(excType, excValue, traceback, logger=logging.getLogger()):
+    logging.error("Logging an uncaught exception",
+                 exc_info=(excType, excValue, traceback))
+
+# Helper function to clean up and alert that we are ending runtime
+@atexit.register
+def shutdown():
+    logging.info(f"{APP_NAME} {APP_VERSION} - Shutting down...")
+
+# Stub code to bootstrap environment and launch into main application
+def run():
+    # Configure logging
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s [%(name)s] [%(funcName)s] [%(levelname)s]  %(message)s",
+        handlers=[
+            logging.StreamHandler(sys.stderr),
+            logging.FileHandler("{0}/{1}.log".format("./", APP_EXE_NAME.lower()))
+        ]
+    )
+
+    # Ensure we capture stack traces in our log
+    sys.excepthook = log_excepthook
+
+    # Announce that we are starting up.
+    logging.info(f'{APP_NAME} {APP_VERSION} - Starting up...')
+
+    # Launch into the main application run code
+    LoanCalc.run()
+
+if __name__ == '__main__':
+    run()
