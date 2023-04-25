6a068ff7376b78f342cd7baea9e4bec007dfbbcd
Corey S. Seliger <corey@seligerfamily.org>
2023-04-25 15:52:57 -0400

    Window WIP
    - Added centering code
    - Scaffolding for frames (need to break out into a class)

--- loancalc/view/windows.py
+++ loancalc/view/windows.py
@@ -15,10 +15,37 @@ class MainAppWindow(tk.Tk):
 
         self.wm_title(f'{loancalc.APP_NAME} {loancalc.APP_VERSION}')
 
-        container = tk.Frame(self, height=900, width=1500)
-        container.pack(side="top", fill="both", expand=True)
-        container.grid_rowconfigure(0, weight=1)
-        container.grid_columnconfigure(0, weight=1)
+        # Calculate geometry and position of the main window
+        screen_width = self.winfo_screenwidth()
+        screen_height = self.winfo_screenheight()
+
+        center_x = screen_width // 2
+        center_y = screen_height // 2
+
+        window_width = screen_width // 2
+        window_height = screen_height // 2
+
+        titlebarHeight = self.winfo_rooty() - self.winfo_y()
+
+        self.geometry('{}x{}+{}+{}'.format(
+            window_width,
+            window_height,
+            center_x - (window_width // 2),
+            center_y - (window_height // 2)
+        ))
+
+        self.resizable(False, False)
+
+        self.grid_rowconfigure(0, minsize=window_height )
+        self.grid_columnconfigure(0, minsize=(window_width // 2.5))
+        self.grid_columnconfigure(1, minsize=(window_width - (window_width // 2.5)))
+
+
+        container = tk.Frame(self, relief=tk.GROOVE, bd=2)
+        container.grid(row=0, column=0, sticky="nsew", padx=10, pady=10)
+
+        container2 = tk.Frame(self, relief=tk.GROOVE, bd=2)
+        container2.grid(row=0, column=1, sticky="nsew", padx=10, pady=10)
 
         self.frames = {}
 
