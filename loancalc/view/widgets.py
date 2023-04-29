52331200be0400aaedf7d4275b8ae3cf2a2786c2
Corey S. Seliger <corey@seligerfamily.org>
2023-04-29 09:54:37 -0400

    StackedEntry
    - Got the proper master<->child relationship figured out between nested widgets (frames)
    - Sorted out sticky and weight settings
    - append_widget is a quasi-decorator method for instantiating widgets inside container classes

--- loancalc/view/widgets.py
+++ loancalc/view/widgets.py
@@ -1,11 +1,45 @@
-
 import tkinter as tk
 import logging
 
 logger = logging.getLogger(__name__)
 
+
 class Container(tk.Frame):
 
-    def __init__(self, column=0, row=0, bd=2, relief=tk.GROOVE):
-        tk.Frame.__init__(self, relief=relief, bd=bd)
-        self.grid(row=row, column=column, sticky='nsew', padx=10, pady=10)
+    widgets = []
+
+    def __init__(self, master, bd=2, relief=tk.GROOVE):
+        tk.Frame.__init__(self, master, relief=relief, bd=bd)
+        self.columnconfigure(0, weight=1)
+        logger.debug(f"{__class__.__name__}'s master: {self.master}")
+
+    def append_widget(self, widget_type, *args, **kwargs):
+        logger.debug(f'{__class__.__name__} is {self}')
+        widget = widget_type(self, *args, **kwargs)
+        self.widgets.append(widget)
+        self.widgets[len(self.widgets) - 1].grid(
+            column=0,
+            row=len(self.widgets) - 1,
+            sticky="nsew",
+            padx=40,
+            pady=20
+        )
+
+
+class StackedEntry(tk.Frame):
+    def __init__(self, master, text, validator=None):
+        tk.Frame.__init__(self, master)
+
+        self.columnconfigure(0, weight=1)
+        self['bg'] = '#ffc0c0'
+
+        self.lbl_entry = tk.Label(self)
+        self.txt_entry = tk.Entry(self)
+
+        self.lbl_entry['text'] = text
+        self.lbl_entry.grid(row=0, column=0, sticky='nsew')
+        self.txt_entry.grid(row=1, column=0, sticky='nsew')
+
+        # Force this widget to set the focus to the entry
+        # box when called.
+        self.focus_set = self.txt_entry.focus_set
--- loancalc/view/windows.py
+++ loancalc/view/windows.py
@@ -35,15 +35,29 @@ class MainAppWindow(tk.Tk):
             center_y - (window_height // 2)
         ))
 
+        # Set the window to be a static size (no re-sizing!)
         self.resizable(False, False)
 
-        self.grid_rowconfigure(0, minsize=window_height )
+        # Configure the grid to have a specific shape (approximately 40/60)
+        self.grid_rowconfigure(0, minsize=window_height)
         self.grid_columnconfigure(0, minsize=(window_width // 2.5))
         self.grid_columnconfigure(1, minsize=(window_width - (window_width // 2.5)))
 
+        # Drop two containers into the grid
+        container = lcw.Container(self, row=0, column=0)
+        container.grid(row=0, column=0, sticky='nsew', padx=10, pady=10)
 
-        container = lcw.Container(row=0, column=0)
-        container2 = lcw.Container(row=0, column=1)
+        container2 = lcw.Container(self, row=0, column=1)
+        container2.grid(row=0, column=1, sticky='nsew', padx=10, pady=10)
+
+        # Sorting out how to add a widget to an existing widget
+        container.append_widget(lcw.StackedEntry, 'Test Entry 1:')
+        container.append_widget(lcw.StackedEntry, 'Test Entry 2:')
+
+        container.widgets[0].focus_set()
+
+        # self.item2 = lcw.StackedEntry('Test entry 2:')
+        # self.item2.grid(row=1, column=0, sticky='new', padx=15, pady=15)
 
         # TODO: Fix this to do something else...
         self.frames = []
